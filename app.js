ace39fe56c50336f91a93eb1c7bca4d381731a1d
trojan081 <trojan081@yandex.ru>
2021-02-22 13:29:07 +0300

    if else ?

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+// --------------------------------------------------------IF ELSE ?---------------------------------
 // let age = prompt ('Сколько Вам лет?', '');
 // let message = (age < 3) ? 'Привет, малыш!' :
 //     (age < 18) ? 'Какой прекрасный день!' :
@@ -15,4 +17,26 @@
 //     message = 'What an unusual age, sir!'
 // };
 // alert (message);
+
+// let name = prompt ('Whats the "official" name of JavaScript?', '');
+//     if (name == 'ECMAScript') {
+//         message = 'Correct!' // или сразу alert ('Correct!')
+//     } else {
+//         message = 'Oh, you don`t know? It`s ECMAScript!' // или сразу alert ('....')
+//     };
+// alert (message);
+
+// let num = prompt ('Введите число', '');
+// if (num > 0) {
+//     alert (1)
+// } else if (num == 0) {
+//     alert (0)
+// } else {alert (-1)}
+
+// let result;
+// result = (a + b < 4) ? 'Мало' : 'Много';
+
+// let message = (login == 'Сотрудник') ? 'Привет' :
+// (login == 'Директор')? 'Здравствуйте' :
+// (login == '') ? 'Нет логина' : '';
 
